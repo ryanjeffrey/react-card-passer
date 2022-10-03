499c31162ea4d8c4724e3d274b2dd2c14be0b7a3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-10-03 17:30:33 -0500

    Refactor using context

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,28 +1,16 @@
 import React from 'react';
+import { useGame } from '../context/GameContext';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  setSelectedCard,
-  selectedCard,
-  to,
-}) {
+export default function Player({ player, hand }) {
+  const { to, setTo } = useGame();
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
       <p>Player {player}</p>
-      <CardList
-        player={player}
-        cards={hand}
-        setFrom={setFrom}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
-      />
+      <CardList player={player} cards={hand} />
     </div>
   );
 }
